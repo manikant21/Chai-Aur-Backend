e168294e5df1ea9a928b9199406b49edad206bcc
Manikant <manikant21022001@gmail.com>
2024-09-13 08:37:25 +0530

    Backend connection

---
+++ src/index.js
@@ -0,0 +1,58 @@
+// require('dotenv').config({path: './env'})  ---It will also work but it is not good way
+
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+ 
+
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----It can also be done.....But This not Good approach
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants.js';
+// import express from 'express';
+// const app = express();
+// ;(async() =>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//        app.on("error", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error;
+//        })
+
+//        app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on ${process.env.PORT}`);
+//        })
+       
+//     }
+//     catch(error){
+//         console.error("Error: " ,error);
+//         process.exit(1);
+//     }
+// })()
