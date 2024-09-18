f950740dc4cf6e9788969fcf1ca7883a73d0b6c1
Manikant <manikant21022001@gmail.com>
2024-09-18 08:45:35 +0530

    utils Folder Setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
  
 
 
@@ -13,7 +14,20 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error: ", error);
+    })
+})
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at PORT: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !! ", err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+
+// Using Promise method 
+const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    }
+}
+
+export {asyncHandler}
+
+
+
+// Another way using async and await 
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     }
+//     catch(error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+
+//     }
+
+// }
